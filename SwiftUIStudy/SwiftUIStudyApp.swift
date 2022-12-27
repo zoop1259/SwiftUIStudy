417472ec94f475f445687598ebc92f1958d649d4
zoop1259 <kdm432191@gmail.com>
2022-12-27 12:49:55 +0900

    Initial Commit

---
+++ SwiftUIStudy/SwiftUIStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStudyApp.swift
+//  SwiftUIStudy
+//
+//  Created by 강대민 on 2022/12/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
