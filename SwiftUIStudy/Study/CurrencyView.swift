585594a597c514e8050b7236918b7957a2312753
zoop1259 <kdm432191@gmail.com>
2022-12-28 11:34:48 +0900

    기본적인 수수료 계산기앱 완료.

---
+++ SwiftUIStudy/Study/CurrencyView.swift
@@ -0,0 +1,80 @@
+//
+//  CurrencyView.swift
+//  SwiftUIStudy
+//
+//  Created by 강대민 on 2022/12/27.
+//
+
+import SwiftUI
+
+struct CurrencyView: View {
+    @State private var checkAmount = 0.0 //수표 비용.
+    @State private var numberOfPeople = 2 //분담 인원
+    @State private var tipPercentage = 20 //남겨야할 팁
+    @FocusState private var amountIsFocused: Bool //키보드 내리기 위한 포커스
+    let tipPercentages = [10, 15, 20, 25, 0] //세그먼트에 추가될 퍼센트.
+
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("통화"), content: {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+                        .keyboardType(.decimalPad)
+                    
+                    
+                    TextField("OldCodeAmount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                })
+
+                Section(header: Text("분담 인원"), content: {
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                })
+                
+                Section {
+                    Picker("Tip", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented) //이렇게하면 세먼트 스타일로 변경된다.
+                } header: {
+                    Text("이런식으로도 섹션의 헤더를 추가할 수 있다.")
+                }
+                
+                Section(header: Text("결과"), content: {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                })
+                .navigationTitle("Split")
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Spacer()
+                        
+                        Button("Done") {
+                            amountIsFocused = false
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct CurrencyView_Previews: PreviewProvider {
+    static var previews: some View {
+        CurrencyView()
+    }
+}
---
+++ SwiftUIStudy/Study/PickerView.swift
@@ -0,0 +1,30 @@
+//
+//  PickerView.swift
+//  SwiftUIStudy
+//
+//  Created by 강대민 on 2022/12/27.
+//
+
+import SwiftUI
+
+struct PickerView: View {
+    let students = ["Harry", "Hermione", "Ron"]  //피커 목록
+    @State private var selectedStudent = "Harry" //베이스값.
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Picker("Select your student", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) { //선언한 학생의 목록을 foreach로 묶고 id값을 self로하여 등록한다.
+                        Text($0) //각 값은 $0으로 하나씩 하나씩 줄줄이 소세지
+                    }
+                }
+            }
+        }
+    }
+}
+struct PickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        PickerView()
+    }
+}
