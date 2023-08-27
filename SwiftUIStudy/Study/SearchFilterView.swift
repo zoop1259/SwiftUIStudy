709ebeb864c987becba862173cf297edf8b838f2
zoop1259 <kdm43211991@gmail.com>
2023-08-27 23:39:41 +0900

    주석 추가.

--- SwiftUIStudy/Study/SearchFilterView.swift
+++ SwiftUIStudy/Study/SearchFilterView.swift
@@ -8,10 +8,18 @@
 import SwiftUI
 
 struct SearchFilterView: View {
-
+    //입력받을 변수
     @State private var searchText = ""
-     let words = ["가지", "나라"]
-
+    //검색 대상으로 사용될 단어 목록.
+    let words = ["가지", "나라"]
+    
+/*
+ 아래의 함수는 searchText를 기반으로 초성부터 필터링된 단어 목록을 반환하는 함수.
+ 초성을 변환한것을 담는 변수를 생성
+ 단어목록을 필터링(클로저를통하여)을 반환함
+ 또 새로운 변수로 초성을 변환한 것을 담는 변수를 하나 더 생성
+ 초성 필터링된 단어와 검색어의 초성을 비교하여 리턴.
+ */
      func filteredWords(_ searchText: String) -> [String] {
          let chosungSearchText = convertToChosung(searchText)
          return words.filter { word in
@@ -36,6 +44,19 @@ struct SearchFilterView: View {
              "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"
          ]
 
+         /*
+          위에서 초성배열을 생성.
+          result로 문자열 초기화
+          함수가 호출될때 입력받은 문자열인 text 반복문 시작.
+          그리고 문자의 유니코드 스칼라 값을 가져온다. 그리고 한글인지 확인하는 조건문 시작.
+          한글의 유니코드 범위에서 초성을 계산하는 수식을 사용하여 저장하는 변수 생성
+          계산된 초성을 추가
+          한글이 아닌 문자의 경우를 계산
+          한글이 아닌문자는 그냥 추가.
+          
+          
+          
+          */
          var result = ""
          for character in text {
              if let scalar = character.unicodeScalars.first, scalar.isHans {
@@ -83,6 +104,14 @@ struct SearchFilterView_Previews: PreviewProvider {
     }
 }
 
+
+/*
+ 
+ 유니코드 스칼라 확장
+ 한글 유니코드 범위인지 확인하는 변수 생성
+ 유니코드 범위에 있는지 확인하고 리턴.
+ 
+ */
 extension UnicodeScalar {
     var isHans: Bool {
         return value >= 0xAC00 && value <= 0xD7A3
