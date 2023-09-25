4167af90ac13ffb295d4f17101facf93a9a62079
zoop1259 <kdm43211991@gmail.com>
2023-09-25 23:12:26 +0900

    Case를 추가하여 연습.

--- SwiftUIStudy/Study/LongPressGuesTureView.swift
+++ SwiftUIStudy/Study/LongPressGuesTureView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct LongPressGuesTureView: View {
     @State private var isUnlocked = false
+    @State private var secUnlocked = false
+    @State private var checkText = ""
     
     var body: some View {
         VStack {
@@ -20,7 +22,28 @@ struct LongPressGuesTureView: View {
                 .onLongPressGesture {
                     self.isUnlocked.toggle()
             }
-        }    }
+            
+            Text("Second lock: \(secUnlocked.description)")
+            Image(systemName: secUnlocked ? "lock" : "lock.open")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 50, height: 50)
+                .onLongPressGesture {
+                    self.secUnlocked.toggle()
+            }
+            
+            Spacer()
+            
+            //State의 값이 변할때마다 view가 새로 그려지기에 그때그때 변한다.
+            if isUnlocked && secUnlocked {
+                Text("잠겼습니다")
+                    .font(.title)
+                    .foregroundColor(.green)
+            } else {
+                Text("둘다 잠궈주세요.")
+            }
+        }
+    }
 }
 
 struct LongPressGuesTureView_Previews: PreviewProvider {
